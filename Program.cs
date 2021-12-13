f8932fdf4210b08cd0d24d0e4ccb14bd6d673e04
Malmsteg <73699667+Malmsteg@users.noreply.github.com>
2021-12-13 22:07:01 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace AdventOfCode_2021_day14
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string file = Path.Combine(currentDirectory, @"..\..\..\input.txt");
+            string path = Path.GetFullPath(file);
+            string[] text = File.ReadAllText(path).Replace("\r", "").Split("\n");
+        }
+    }
+}
