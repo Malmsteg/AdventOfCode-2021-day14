e81a9b7bc65a7deb1c87a4886e69933a440a24c0
Malmsteg <73699667+Malmsteg@users.noreply.github.com>
2021-12-14 22:11:02 +0100

    First part done

--- Program.cs
+++ Program.cs
@@ -13,6 +13,57 @@ namespace AdventOfCode_2021_day14
             string file = Path.Combine(currentDirectory, @"..\..\..\input.txt");
             string path = Path.GetFullPath(file);
             string[] text = File.ReadAllText(path).Replace("\r", "").Split("\n");
+            string input = text[0];
+            Dictionary<string,string> polymer = new();
+            Dictionary<char, long> result = new();
+
+            for(int i = 2; i < text.Length; i++)
+            {
+                string[] temp = text[i].Split(" -> ");
+                polymer.Add(temp[0],temp[1]);
+            }
+
+            string str = new(input);
+            int count = 0;
+
+            for(;count < 10; count++)
+            {
+                string next = "";
+                for(int i = 0; i < str.Length;i++)
+                {
+                    if(i != str.Length-1)
+                    {
+                        string temp = "" + str[i] + str[i+1];
+                        if(polymer.ContainsKey(temp))
+                        {
+                            next += str[i] + polymer[temp];
+                        }
+                        else
+                        {
+                            next+= str[i];
+                        }
+                    }
+                    else
+                    {
+                        next+= str[i];
+                    }
+                }
+                str = new (next);
+            }
+            Console.WriteLine(str.Length);
+            for(int i = 0; i < str.Length; i++)
+            {
+                if(!result.ContainsKey(str[i]))
+                {
+                    result.Add(str[i],0);
+                }
+                result[str[i]]++;
+            }
+            Console.WriteLine(result.Values.Max() - result.Values.Min());
+            foreach(var item in result)
+            {
+                Console.WriteLine($"{item.Key} equals {item.Value}");
+            }
         }
     }
 }
